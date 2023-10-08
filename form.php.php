0d26c93bbeaffdd5d4b827ff617a824f7be8719d
steynsk <140870572+steynsk@users.noreply.github.com>
2023-10-08 11:45:25 +0200

    Update form.php.php
    The form was not working

--- form.php.php
+++ form.php.php
@@ -4,9 +4,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
+    // Validate and sanitize user input
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $message = filter_var($message, FILTER_SANITIZE_STRING);
+
+    // Check for valid email address
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address. Please go back and try again.";
+        exit;
+    }
+
     // Specify your email address where you want to receive form submissions
     $to = "michaelweston175@gmail.com";
-    
+
     // Subject of your email
     $subject = "New Business Description Request from $name";
 
@@ -16,13 +27,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_content .= "Message:\n$message";
 
     // Additional headers
-    $headers = "From: $name <$email>";
+    $headers = "From: $email";
 
     // Send the email
-    mail($to, $subject, $email_content, $headers);
-
-    // Redirect back to the thank you page
-    header("Location: thank_you.html");
+    if (mail($to, $subject, $email_content, $headers)) {
+        // Redirect to a thank you page
+        header("Location: thank_you.html");
+        exit;
+    } else {
+        echo "An error occurred while sending the email. Please try again later.";
+    }
 } else {
     // If this page is accessed directly, do something (e.g., show an error message)
     echo "Direct access not allowed!";
